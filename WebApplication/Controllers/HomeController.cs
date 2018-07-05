5c19ec043eb3da1a0588ef62ceceb66e81f00ed4
jbelodelf <jbelodelf@gmail.com>
2018-07-05 07:46:50 -0300

    Update HomeController.cs

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -4,34 +4,73 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication.Models;
+using System.Data.Entity;
 
 namespace WebApplication.Controllers
 {
-    public class HomeController : Controller
+        public class HomeController : Controller
     {
         public ActionResult Index()
         {
             var amigos = new List<AmigoEntity>();
-            using (var contecto = new myDbContexto())
+            using (var contexto = new myDbContexto())
             {
-                amigos = contecto.Amigo.ToList();
+                amigos = contexto.Amigo.ToList();
             }
-
             return View(amigos);
         }
 
-        public ActionResult About()
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Title = "Create";
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AmigoEntity amigo)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            using (var contexto = new myDbContexto())
+            {
+                contexto.Amigo.Add(amigo);
+                contexto.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
+
+        public ActionResult Edit(int id)
+        {
+            var amigo = new AmigoEntity();
+            using (var contexto = new myDbContexto())
+            {
+                amigo = contexto.Amigo.FirstOrDefault(a => a.IdAmigo == id);
+            }
+            return View(amigo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(AmigoEntity amigo)
+        {
+            using (var contexto = new myDbContexto())
+            {
+                contexto.Entry(amigo).State = EntityState.Modified;
+                contexto.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var contexto = new myDbContexto())
+            {
+                var amigo = contexto.Amigo.Find(id);
+                contexto.Entry(amigo).State = EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
